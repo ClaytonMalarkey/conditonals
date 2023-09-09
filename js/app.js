6ea5266d09a7613ff506753be219a2a8f7cf78a5
ClaytonMalarkey <malarkey1985@gmail.com>
2023-09-08 18:55:45 -0600

    forth commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 var a = 60;
 if (a >= 75){
     console.log("great");
+}else if(a < 75 && a >= 50) {
+    console.log("keep going")
 }else {
-    console.log("to bad")
+    console.log("try again");
 }
